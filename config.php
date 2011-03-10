336b023f766cb649aae821b7a130a7ddfc4d6ee0
Dmitry Filimonov <dmitry@sliday.com>
2011-03-10 23:42:55 +0300

    Added config.php

---
+++ config.php
@@ -0,0 +1,25 @@
+﻿<?php
+//Meta tags
+$copyright='2011 Sliday';
+$language='English';
+$description='flash SEO Test page';
+$keywords='flash, SEO, Sliday';
+$author='Dmitry Filimonov';
+
+//Page title. Leave it blank if you define it on the page
+//Заголовок страницы - если вы указываете его непосредственно на странице сайта, оставьте поле пустым
+$title='';
+
+//Paths to the text data files. Use comma as a delimeter
+//Пути к xml-файлам с текстовым содержимым. Для разделения используйте запятую
+$text_files='data.xml';
+
+//Paths to the image data files. Use comma as a delimeter
+//Пути к xml-файлам с графическим содержимым. Для разделения используйте запятую
+$img_files='img.xml';
+
+
+//
+//Префикс используется, если пути к картинкам в xml-файле прописаны не от того места где лежит файл flashSEO.php
+$img_prefix='';
+?>

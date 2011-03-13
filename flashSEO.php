43e4e67d6c65dd215895a61c7b3b0300ef3a9326
Dmitry Filimonov <dmitry@sliday.com>
2011-03-13 03:06:40 +0300

    Full XSL support

--- flashSEO.php
+++ flashSEO.php
@@ -1,23 +1,25 @@
 ﻿<?php 
 /*-----------------------------
 SEO for flash - © Sliday 2011
-Read README.txt file for help
-version 1.00	24.02.2011
+Read README file for help
+
+Author - Filimonov Dmitry
+version 1.1    13.03.2011
 -----------------------------*/
 
-//Проверка на бота
+
 $bot=false;
 $test_mode=false;
+
+//Bot checking
 $bot_aliases=array('googlebot','YandexBot','MSNBot','Mail.Ru');
 if(isset($_SERVER['HTTP_USER_AGENT'])){
 	for($i=0;$i<count($bot_aliases);$i++)
-	$bot = $bot||(false!==strripos ($_SERVER['HTTP_USER_AGENT'],$bot_aliases[$i]));	
+	$bot = $bot||(false!==strripos ($_SERVER['HTTP_USER_AGENT'],$bot_aliases[$i])); 
 }
 
-//Проверка на тестовый режим
 if(isset($_GET['getmeta'])||isset($_GET['getcontent'])||isset($_GET['testseo']))$test_mode=true;
 
-//Объявление переменных
 $copyright='';
 $language='';
 $description='';
@@ -28,12 +30,12 @@ $text_files='';
 $img_files='';
 $img_prefix='';
 
-//Проверка наличия кофигурационного файла, его загрузка
+//Config loading
 if (!@include_once('config.php')){ 
 	exit('Can\'t find file "config.php" <br>Be sure, that you placed it in the same folder with "flashSEO.php"<br>For further information please read "README" ');
 }
 
-//генерирование мета тэгов
+//Generating meta-tags
 $meta_str=
 '<meta name="robots" content="ALL" /> 
 <meta name="revisit-after" content="10 days" /> 
@@ -44,6 +46,9 @@ $meta_str=
 <meta name="document-distribution" content="Global" /> 
 <meta name="document-state" content="Static" />
 ';
+function addMeta($key,$value){
+	if($value!='')$GLOBALS["meta_str"].="\n<meta name=\"$key\" content=\"$value\" />";
+}
 addMeta('copyright',$copyright);
 addMeta('language',$language);
 addMeta('description',$description);
@@ -51,122 +56,67 @@ addMeta('keywords',$keywords);
 addMeta('author',$author);
 if($title!='')$meta_str.="\n<title>$title</title>";
 
-//генерирование основного текста
 $content_str='';
-if($title!='')$content_str.="<h1>$title</h1>\n";
 
+if(!($bot||$test_mode)){
+	$content_str="\n\n\n<!--!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nATTENTION\nTEXT AND IMAGES WILL ONLY BE SHOWN FOR SEARCH BOTS\nIF YOU WANT TO SEE IT LIKE A SEARCH BOT, ADD ?testseo TO YOUR URL\nLIKE THIS: http://www.example.com/index.php?testseo\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!-->\n\n\n";
+}
 
-
-
-
-if($bot||$test_mode){
-	if($text_files!=''){
-		$text_files_arr=explode(',',$text_files);
+//main parsing function
+function parseXML($files_str,$img_flag=0){
+	if($files_str!=''){
+		$text_files_arr=explode(',',$files_str);
 		for($i=0;$i<count($text_files_arr);$i++){
 			$doubledot_pos=strrpos($text_files_arr[$i],":");
 			$file_name='';
 			$xsl_name='';
-			if($doubledot_pos===false)
+			if($doubledot_pos===false){
 				$file_name=trim($text_files_arr[$i]);
-			else{
+			}else{
 				$filenames_arr=explode(':',$text_files_arr[$i]);
 				$file_name=trim($filenames_arr[0]);
 				$xsl_name=trim($filenames_arr[1]);
 			}
 			if($file_name!=''){
+			
+				$xml = new DOMDocument;
+				if(!file_exists($file_name))
+				exit('Can\'t find file "'.$file_name.'" <br>Be sure, that it exists and that you edited your config.php right <br>For further information please read "README" ');
+				$xml->load($file_name);
+				$xsl = new DOMDocument;
 				if($xsl_name!=''){
-					$xml = new DOMDocument;
-					if(!file_exists($file_name))
-						exit('Can\'t find file "'.$file_name.'" <br>Be sure, that it exists and that you edited your config.php right <br>For further information please read "README.txt" ');
-					$xml->load($file_name);
-					$xsl = new DOMDocument;
 					if(!file_exists($xsl_name))
-						exit('Can\'t find file "'.$xsl_name.'" <br>Be sure, that it exists and that you edited your config.php right <br>For further information please read "README.txt" ');
+					exit('Can\'t find file "'.$xsl_name.'" <br>Be sure, that it exists and that you edited your config.php right <br>For further information please read "README" ');
 					$xsl->load($xsl_name);
 					$proc = new XSLTProcessor;
 					$proc->importStyleSheet($xsl); 
-
-					$content_str.=str_replace("&gt;",">",str_replace("&lt;","<",$proc->transformToXML($xml)));
+					$GLOBALS["content_str"].=str_replace("&gt;",">",str_replace("&lt;","<",$proc->transformToXML($xml)));
 				}else{
-				$xml = new DOMDocument;
-					if(!file_exists($file_name))
-						exit('Can\'t find file "'.$file_name.'" <br>Be sure, that it exists and that you edited your config.php right <br>For further information please read "README.txt" ');
-					$xml->load($file_name);
-					$xsl = new DOMDocument;		
-					$xsl->loadXML( $default_xls );					
+					$xsl->loadXML($img_flag==1?$GLOBALS['default_img_xls']:$GLOBALS['default_text_xls']);                                        
 					$proc = new XSLTProcessor;
 					$proc->importStyleSheet($xsl); 
-
-					$content_str.=str_replace("&gt;",">",str_replace("&lt;","<",$proc->transformToXML($xml)));
+					$GLOBALS["content_str"].=str_replace("&gt;",">",str_replace("&lt;","<",$proc->transformToXML($xml)));
 				}
 			}
 		}
-	}
-	if($img_files!=''){
-		$img_files_arr=explode(',',$img_files);
-		for($i=0;$i<count($img_files_arr);$i++){
-			$file_name=trim($img_files_arr[$i]);
-			if($file_name!=''){
-			if(!file_exists($file_name))
-				exit('Can\'t find file "'.$file_name.'" <br>Be sure, that it exists and that you edited your config.php right <br>For further information please read "README.txt" ');
-			if(!$xml = simplexml_load_file($file_name,NULL,LIBXML_NOCDATA|LIBXML_NOBLANKS))
-				exit("simplexml error\nBe sure, that your xml files are written in UTF-8");
-			$str='';
-			parseImg($xml,$str);
-			$GLOBALS["content_str"].=$str;
-			}
-		}
-	}
-}else{
-	$GLOBALS["content_str"]="\n\n\n<!--!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nATTENTION\nTEXT AND IMAGES WILL ONLY BE SHOWN FOR SEARCH BOTS\nIF YOU WANT TO SEE IT LIKE A SEARCH BOT, ADD ?testseo TO YOUR URL\nLIKE THIS: http://www.example.com/index.php?testseo\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!-->\n\n\n";
+	}	
 }
 
-function parseXSLT($xml,&$str){
-	
+if(isset($_GET['getmeta'])){
+	exit($meta_str);
 }
-
-
-
-//функция парсинга файлов с картинками
-function parseImg($xml,&$str){
-	$tmp;
-	foreach($xml->children() as $tmp){
-		$arr=$tmp->attributes();
-		if(isset($arr['alb_title'])&&isset($arr['alb_description'])){
-			$str.= "<h2>".$arr['alb_title']."</h2>\n";
-			$str.= ("<p>".$arr['alb_description']."</p>\n\n");
-		}
-		foreach($tmp->children() as $tmp2){
-			$arr2=$tmp2->attributes();
-			if(isset($arr2['img_title'])&&isset($arr2['img_description'])&&isset($arr2['img_src'])){
-				$str.= "<p><img src='".$GLOBALS['img_prefix'].$arr2['img_src']."' alt='".$arr2['img_title']."'>\n";
-				$str.= ("".$arr2['img_description']."</p>\n\n");
-				
-			}
-		}
-	}
+if(isset($_GET['getcontent'])){
+	parseXML($text_files);
+	parseXML($img_files,1);
+	exit($content_str);
 }
-
-$html_head="<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n</head>";
-
-if(isset($_GET['getmeta']))
-	exit( $GLOBALS["html_head"].str_replace(">","&gt;<br>",str_replace("<","&lt;",$GLOBALS["meta_str"]))."</html>");
-if(isset($_GET['getcontent']))
-	exit($GLOBALS["content_str"]);
-	//exit( $GLOBALS["html_head"].str_replace(">","&gt;<br>",str_replace("<","&lt;",$GLOBALS["content_str"]))."</html>");
 
 //вспомогательные функции
-function addMeta($key,$value){
-	if($value!='')$GLOBALS["meta_str"].="\n<meta name=\"$key\" content=\"$value\" />";
-}
 function getMeta(){
-	echo $GLOBALS["meta_str"];
-	
+	echo $GLOBALS["meta_str"];	
 }
 function getContent(){
-	echo $GLOBALS["content_str"];
-	
+	echo $GLOBALS["content_str"];	
 }
-
 
 ?>

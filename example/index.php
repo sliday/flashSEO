f4d928f87433cceedd1a1eb72dd0f22807c5fe6e
Dmitry Filimonov <dmitry@sliday.com>
2011-03-29 00:04:00 +0400

    fixed UTF-8 BOM

--- example/index.php
+++ example/index.php
@@ -1,4 +1,4 @@
-﻿<?php require_once('config.php'); ?>
+<?php require_once('config.php'); ?>
 <?php require_once('../src/flashSEO.php'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"> 

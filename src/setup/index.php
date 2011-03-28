f001cb14e146780352d157c3dc3bb40b142e6eaa
Dmitry Filimonov <dmitry@sliday.com>
2011-03-28 23:57:22 +0400

    fixed UTF-8 BOM

--- src/setup/index.php
+++ src/setup/index.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<?php
+	if (!@include_once('../config.php')){ 
+		exit('Can\'t find file "config.php" <br>Be sure, that you placed it in the same folder with "flashSEO.php"<br>For further information please read "README" ');
+	}
+?>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"> 
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
@@ -12,8 +17,11 @@
 			h1,h2{
 				font-weight:normal;
 			}
+			h2{
+				margin-top:0px;
+				margin-bottom:0px;
+			}
 			.left_panel a{
-		
 				font-size:20px;
 			}
 			.left_panel{
@@ -28,8 +36,14 @@
 				width:400px;
 			}
 			.links {
+				width:100%;
+				//position:absolute;
+				//bottom:20px;
+				padding-top:20px;
+				//border-top:1px solid #cccccc;
 				
-				margin-top:30px;
+				padding-left:8px;
+				//margin-top:30px;
 				font-size:12px;
 			}
 			.links div {
@@ -38,6 +52,7 @@
 			.meta_title{
 				padding-top:4px;
 				margin-bottom:-4px;
+				
 			}
 			.meta_description{
 				font-size:11px;
@@ -51,67 +66,72 @@
 		</style>
 	</head> 
 	<body> 
-		<table>
+		<table style="padding-bottom:20px;">
 			<tr><td style="vertical-align:top;">
 					<div class="left_panel">
 						<a href="index.php" ><img src="flashSEO_logo.png" width="200px" height="51px" ></a>
-						<div style="text-align:right;margin-right:10px;margin-top:-10px;margin-bottom:-29px"><i>by Sliday</i></div>
+						<div style="text-align:right;margin-right:10px;margin-top:-10px;margin-bottom:-8px"><i>by Sliday</i></div><!--margin-bottom:-29px-->
 						<br><br>
 						<a href="index.php?basic" >Basic settings</a><br><br>
 						<a href="index.php?patterns" >Patterns</a>
 					</div>
 			</td>
 			
-			<td style="display: block;margin-top:-2px;padding-left:15px;padding-top:2px;border-left:1px solid #cccccc">
+			<td style="display: block;margin-top:-2px;margin-bottom:-2px;padding-bottom:30px;padding-left:15px;padding-top:2px;border-left:1px solid #cccccc">
 			<div >
 					<h1>Basic settings</h1>
 					
 					<div style="padding-left:20px;">
-					<table>
-						<tr>
-							<td width="50%" style="text-align:center">
+								<div style="width:400px;text-align:center;">
 								<h2>Meta-information</h2>
-							</td>
-							<td width="400px" style="text-align:center">
-								<h2>XML-files</h2>
-							</td>
-						</tr>
-						<tr><td>
+								</div>
 							<?php
-							$titles=array("Title","Copyright","Language","Description","Keywords","Author");
-							$descriptions=array("The title of your page. Leave it blank if you already defined it on the page.","A copyright statement.","The main language of your site.","A short description of the page.","Words that identify what the page is about","The author's name and possibly email address.");
-							for($i=0;$i<count($titles);$i++){
-							$r='';
-							if($i%2==1){
-								$r="style='background-color:#ffcccc;'";
-							}else{
-								$r="style='background-color:#ccffcc;'";
-							}
-							$r='';
-							echo "<div class='meta_title'>".$titles[$i]."</div><div class='meta_description'>".$descriptions[$i]."</div>";
-							echo "<input class='settings' type='text' />";
-							}
+								$titles=array("Title","Copyright","Language","Description","Keywords","Author");
+								$descriptions=array("The title of your page. Leave it blank if you already defined it on the page.","A copyright statement.","The main language of your site.","A short description of the page.","Words that identify what the page is about","The author's name and possibly email address.");
+								$values=array(&$title,&$copyright,&$language,&$description,&$keywords,&$author);
+								for($i=0;$i<count($titles);$i++){
+								$r='';
+								if($i%2==1){
+									$r="style='background-color:#ffcccc;'";
+								}else{
+									$r="style='background-color:#ccffcc;'";
+								}
+								$r='';
+								echo "<div class='meta_title'>".$titles[$i]."</div><div class='meta_description'>".$descriptions[$i]."</div>";
+								echo "<input class='settings' type='text' value='".$values[$i]."'/>";
+								}
 							?>
 							
-						</td>
-							
-							
-						</tr>
-					</table>
 						
-						<div class="links">
-								<center>
-									<table><tr><td >
-									<a href="http://sliday.com/flashseo-download">flashSEO home page</a>&nbsp;|&nbsp;<a href="https://github.com/sliday/flashSEO">flashSEO on github.com</a>&nbsp;|&nbsp;</td><td><div><a href="http://sliday.com/flashseo-download"><img style="border:0;align:bottom;" src="sliday_logo.png" height="16px"></img ></div></a>
-									</td></tr></table>
-								</center>
-							</div>
+						
 						
 					</div>		
+					
+				
+					
+					
+						
 			</div>
 			</td>
 			</tr>		
+			<!--
+			
+			<tr><td colspan="2" style=" ">
+			<div class="links">
+								<center>
+									<table><tr><td >
+									<a href="http://sliday.com/flashseo-download">flashSEO home page</a>&nbsp;|&nbsp;<a href="https://github.com/sliday/flashSEO">flashSEO on github.com</a>&nbsp;|&nbsp;</td><td><div><a href="http://sliday.com/flashseo-download"><img style="border:0;align:bottom;" src="sliday_logo.png" width="57px" height="17px"></img ></div></a>
+									</td></tr></table>
+								</center>
+						</div>
+			
+			</td></tr>
+			-->
 		</table>
+		
+		
+		
+		
 	</body>
 </html>
 
